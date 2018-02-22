decb5700bbd87ddcd637a1e91336dbbc9f41322b
Jim Speaker <jim@IONTECH.org>
2018-02-21 20:30:43 -0800

    Add blank solution ready for fizz buzz kata.

---
+++ FizzBuzz-20180222-1/BarTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FizzBuzz_20180222_1
+{
+    [TestFixture]
+    public class BarTests
+    {
+        [Test, Category("Unit")]
+        public void GivenFoo_WhenBarring_ThenItShouldBaz()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            1.ToString().Should().Be("1");
+        }
+    }
+}
---
+++ FizzBuzz-20180222-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz_20180222_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz_20180222_1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("482ddee8-5696-4b9f-a32f-f3f35561975d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
